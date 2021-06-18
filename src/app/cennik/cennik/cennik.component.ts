e5146716a8d862f46c9b3adfd1d7cc3e0c70e9a8
Mikołaj Szymkowiak <mikolajszymkowiak@o2.pl>
2021-06-18 11:55:33 +0200

    NajnowszaWersja

--- src/app/cennik/cennik/cennik.component.ts
+++ src/app/cennik/cennik/cennik.component.ts
@@ -1,3 +1,4 @@
+import { ConditionalExpr } from '@angular/compiler';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -7,11 +8,57 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class CennikComponent implements OnInit {
 
-  @ViewChild('input1') input1: ElementRef;
+  constructor() {}
 
-  constructor() { }
+  @ViewChild('input1') input1!: ElementRef;
+  @ViewChild('input2') input2!: ElementRef;
+  @ViewChild('input3') input3!: ElementRef;
+  @ViewChild('input4') input4!: ElementRef;
+  @ViewChild('input5') input5!: ElementRef;
+  @ViewChild('input6') input6!: ElementRef;
+
+  cena1: number = 25;
+  cena2: number = 40;
+  cena3: number = 35;
+  cena4: number = 30;
+  cena5: number = 20;
+  cena6: number = 60;
 
   ngOnInit(): void {
   }
+
+  oblicz1(): void
+  {
+    console.log(this.input1!.nativeElement.value);
+
+  }
+  oblicz2(): void
+  {
+    console.log(this.input2!.nativeElement.value);
+
+  }
+  oblicz3(): void
+  {
+    console.log(this.input3!.nativeElement.value);
+
+  }
+  oblicz4(): void
+  {
+    console.log(this.input4!.nativeElement.value);
+
+  }
+  oblicz5(): void
+  {
+    console.log(this.input5!.nativeElement.value);
+
+  }
+  oblicz6(): void
+  {
+    console.log(this.input6!.nativeElement.value);
+
+  }
+
+
+
 
 }
